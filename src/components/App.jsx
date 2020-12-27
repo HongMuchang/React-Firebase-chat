567dca39817ee07b3e5ff72db7446f191abcbaa8
HongMuchan <muchang0824@gmail.com>
2020-12-27 11:50:26 +0900

    [ADD]configファイルの設定(signin操作)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import Sginin from "./Signin";
 import Main from "./Main";
+import Config from "../config.json";
 
 export default () => {
   const [name, setName] = useState("");
   console.log({ name });
 
-  if (name === "") {
+  if (Config.signInEnabled && name === "") {
     return <Sginin setName={setName} />;
   } else {
     return <Main />;
