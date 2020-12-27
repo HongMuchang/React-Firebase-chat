185ecb2fd38d5c4e1dffeae0d20cd0d15bc9e66c
HongMuchan <muchang0824@gmail.com>
2020-12-27 11:40:47 +0900

    [ADD]日本語対応(変換)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import Sginin from "./Signin";
+import Main from "./Main";
 
 export default () => {
   const [name, setName] = useState("");
   console.log({ name });
 
-  return (
-    <div>
-      <Sginin setName={setName} />
-    </div>
-  );
+  if (name === "") {
+    return <Sginin setName={setName} />;
+  } else {
+    return <Main />;
+  }
 };
--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -3,8 +3,6 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
@@ -56,6 +54,8 @@ export default function SignIn({ setName }) {
   const [disabled, setDisabled] = useState(true);
   //formの値
   const [string, setString] = useState("");
+  //今、日本語入力かの判断
+  const [isComposed, setIsComposed] = useState(false);
 
   //ボタンの有効化、無効化切替
   useEffect(() => {
@@ -83,11 +83,15 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown={(e) => {
+              if (isComposed) return;
               if (e.key === "Enter") {
                 setName(e.target.value);
                 e.preventDefault();
               }
             }}
+            //変換Enter設定
+            onCompositionStart={() => setIsComposed(true)}
+            onCompositionEnd={() => setIsComposed(false)}
           />
           <Button
             type="button"
